abecf1ed5c302e4f51a614c7ef74fddbb71e94a4
fauziyah <fauziyaturrohmah43188@gmail.com>
2019-10-11 05:41:21 -0700

    Create aritmatika.php

---
+++ uus/aritmatika.php
@@ -0,0 +1,16 @@
+<h1>Aritmatika PHP</h1>
+
+<?php
+
+function perkalian($angka, $angka1){
+    return $angka * $angka1;
+}
+
+echo 'hasil perkalian :'. perkalian(20,20).'<br>';
+
+function pembagian($angka, $angka1){
+    return $angka / $angka1;
+}
+
+echo 'hasil pembagian :'. pembagian(100,5);
+?>
