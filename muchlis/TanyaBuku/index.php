00d2b80c3c789b3ee9293fbfb60899ba17de35a2
Haroen <elmahdi869@yahoo.co.id>
2019-11-14 12:34:42 +0700

    add css

---
+++ muchlis/TanyaBuku/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tanya Buku</title>
+	<link rel="stylesheet" href="admin/assets/css/bootstrap.css">
+</head>
+<body>
+
+
+<!-- navbar -->
+<nav class="navbar navbar-light" style="background-color: #e3f2fd;">
+<div class="container">
+
+	<ul class="nav navbar-nav">
+		<li><a href="index.php">HOME</a></li>
+		<li><a href="keranjang.php">KERANJANG</a></li>
+		<li><a href="login.php.php">LOGIN</a></li>
+		<li><a href="checkout.php">CHECKOUT</a></li>
+	</ul>
+</div>	
+
+</body>
+</html>
