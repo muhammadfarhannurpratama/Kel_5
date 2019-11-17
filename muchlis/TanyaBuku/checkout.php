f5557a8a9e7d773fa29925c88ea7cf3d528eee37
Haroen <elmahdi869@yahoo.co.id>
2019-11-18 02:24:56 +0700

    new checkout.php
    query checkout.php

--- muchlis/TanyaBuku/checkout.php
+++ muchlis/TanyaBuku/checkout.php
@@ -81,6 +81,122 @@ if (!isset($_SESSION['pelanggan']))
       	  <?php endif ?>          
         </ul>
       </nav><!-- #nav-menu-container -->
+
+
+      <section class="konten">
+    <div class="container">
+      <br>
+      <br>
+      <br>
+      <h3>Keranjang Belanja</h3>
+      <hr>
+      <table class="table table-bordered">
+        <thead>
+          <tr>
+            <th>No</th>
+            <th>Produk</th>
+            <th>Harga</th>
+            <th>Jumlah</th>
+            <th>SubHarga</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php $nomor=1; ?>
+          <?php $totalbelanja=0; ?>
+          <?php foreach ($_SESSION["keranjang"] as $id_produk => $jumlah): ?>
+          <!-- menampilkan produk yg sedak diperulangkan berdasarkan id_roduk -->
+          <?php
+           $ambil=$koneksi->query("SELECT * FROM produk WHERE id_produk='$id_produk'");
+           $pecah=$ambil->fetch_assoc();
+           $subharga=$pecah['harga_produk']*$jumlah;
+           ?>
+          <tr>
+            <td><?php echo $nomor; ?></td>
+            <td><?php echo $pecah['nama_produk']; ?></td>
+            <td>Rp. <?php echo number_format($pecah['harga_produk']); ?></td>
+            <td><?php echo $jumlah; ?></td>
+            <td>Rp. <?php echo number_format($subharga); ?></td>
+          </tr>
+          <?php $nomor++; ?>
+          <?php $totalbelanja+=$subharga; ?>
+          <?php endforeach ?>
+        </tbody>
+        <tfoot>
+          <tr>
+            <th colspan="4">Total Belanja</th>
+            <th>Rp. <?php echo number_format($totalbelanja) ?> </th>
+          </tr>
+        </tfoot>
+      </table>
+
+      <form method="post">  
+        
+        <div class="row">
+          <div class="col-md-4">
+            <div class="form-group">
+              <input type="text" readonly value="<?php echo $_SESSION['pelanggan']['nama_pelanggan'] ?>" class="form-control"></div>
+          </div>
+          <div class="col-md-4">
+            <div class="form-group">
+              <input type="text" readonly value="<?php echo $_SESSION['pelanggan']['telepon_pelanggan'] ?>" class="form-control">
+            </div>
+          </div>
+          <div class="col-md-4">
+            <select class="form-control" name="id_ongkir">
+              <option value="">Pilih Ongkos Kirim</option>
+              <?php 
+                $ambil=$koneksi->query("SELECT * FROM ongkir");
+                while($perongkir=$ambil->fetch_assoc()){
+               ?>
+               <option value="<?php echo $perongkir['id_ongkir'] ?>">
+                 <?php echo $perongkir['nama_kota'] ?> -
+                 Rp. <?php echo number_format($perongkir['tarif']) ?>
+               </option>
+              <?php } ?>
+            </select>
+          </div>
+        </div>
+        <button class="btn btn-primary " name="checkout">Checkout</button>
+      </form>
+
+      <?php 
+      if (isset($_POST['checkout'])) 
+      {
+        $id_pelanggan=$_SESSION['pelanggan']['id_pelanggan'];
+        $id_ongkir=$_POST['id_ongkir'];
+        $tanggal_pembelian=date("Y-m-d");
+
+        $ambil=$koneksi->query("SELECT * FROM ongkir WHERE id_ongkir='$id_ongkir'");
+        $arrayongkir=$ambil->fetch_assoc();
+        $tarif=$arrayongkir['tarif'];
+
+        $total_pembelian=$totalbelanja + $tarif;
+        # 1. simpan data ke tabel pembelian
+        $koneksi->query("INSERT INTO pembelian (id_pelanggan, id_ongkir, tanggal_pembelian, total_pembelian )
+          VALUES('$id_pelanggan','$id_ongkir','$tanggal_pembelian','$total_pembelian')");
+
+        # 2. mendapatkan id_pembelian yg baru saja terjadi
+        $id_pembelian_baru_terjadi=$koneksi->insert_id;
+
+        foreach ($_SESSION['keranjang'] as $id_produk => $jumlah) 
+        {
+          $koneksi->query("INSERT INTO pembelian_produk (id_pembelian,id_produk,jumlah)
+            VALUES ('$id_pembelian_baru_terjadi','$id_produk','$jumlah')");
+        }
+
+        #kosongkan keranjang belanja
+        unset($_SESSION['keranjang']);
+
+        #tampilan dialihkan ke nota, nota pembelian baru terjadi
+        echo"<script>alert('Pembelian Berhasil !');</script>";
+        echo"<script>location='nota.php?id=$id_pembelian_baru_terjadi';</script>";
+      }
+
+      ?>
+    </div>
+  </section>
+
+
     </div>
   </header>
 
