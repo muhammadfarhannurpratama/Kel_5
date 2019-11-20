49d03595c6f360ec9c0b58009e1c6b2bed6d0d12
tahajjudinfajri <kleponmns@gmail.com>
2019-11-20 08:01:22 +0700

    update

--- tanyabuku/index.php
+++ tanyabuku/index.php
@@ -78,7 +78,7 @@ $koneksi = new mysqli("localhost","root","","db_tanyabuku");
       <h2>Merupakan Situs Belanja Buku Online terUpdate & terPercaya..</h2>
       <img src="admin/assetss/img/logotanyabukupercobaan.png" alt="Hero Imgs">
       <a href="#video" class="btn-get-started scrollto">video</a>
-      <br><br><br>
+      
     </div>
   </section>
 
