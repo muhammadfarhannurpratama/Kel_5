832380df22cfd56cd607b14f98b15106128a1443
Haroen <elmahdi869@yahoo.co.id>
2019-11-24 03:06:06 +0700

    Merapikan Coding

--- tanyabuku/checkout.php
+++ tanyabuku/checkout.php
@@ -50,37 +50,7 @@ if (!isset($_SESSION['pelanggan']))
 
 <body>
 
-  <header id="header" class="header header-hide">
-    <div class="container">
-
-      <div id="logo" class="pull-left">
-        <h1><a href="index.php" class="scrollto"><span>T</span>anya<span>B</span>uku</a></h1>
-        <!-- Uncomment below if you prefer to use an image logo -->
-        <!-- <a href="#body"><img src="img/logo.png" alt="" title="" /></a>-->
-      </div>
-
-      <nav id="nav-menu-container">
-        <ul class="nav-menu">
-          <li class="menu-active"><a href="index.php">Home</a></li>
-          <li><a href="index.php">About</a></li>
-          <li><a href="index.php#team">Best Seller</a></li>
-           <li><a href="index.php#screenshots">Testimoni</a></li>
-          <li><a href="checkout.php">Checkout</a></li>
-          <!--jika sudah login (ada SESSION pelanggan)-->
-          <?php if (isset($_SESSION['pelanggan'])): ?>
-          	<li><a href="logout.php">Logout</a></li>
-          <!--jika belum login ( belum ada SESSION pelanggan)-->
-		  <?php else :  ?>
-		  <li class="menu-has-children"><a href="">Daftar</a>
-            <ul>
-              <li><a href="#">Daftar</a></li>
-              <li><a href="login.php">Masuk</a></li>
-            </ul>
-          </li>	
-      	  <?php endif ?>          
-        </ul>
-      </nav><!-- #nav-menu-container -->
-
+  <?php include 'navbar.php'; ?>
 
       <section class="konten">
     <div class="container">
--- tanyabuku/index.php
+++ tanyabuku/index.php
@@ -37,39 +37,7 @@ include 'koneksi.php';
 
 <body class="body2">
 
-  <header id="header" class="header header-hide">
-    <div class="container">
-
-      <div id="lgoo" class="pull-left">
-        <h1><a href="index.php" class="scrollto"><span>T</span>anya<span>B</span>uku</a></h1>
-        <!-- Uncomment below if you prefer to use an image logo -->
-        <!-- <a href="#body"><img src="img/logo.png" alt="" title="" /></a>-->
-      </div>
-<!-- #nav-menu-container -->
-      <nav id="nav-menu-container">
-        <ul class="nav-menu">
-          <li class="menu-active"><a href="index.php">Home</a></li>
-          <li><a href="#about-us">About</a></li>
-          <li><a href="#team">Best Seller</a></li>
-          <li><a href="#screenshots">Testimoni</a></li>
-          <li><a href="checkout.php">Checkout</a></li>
-          <!--jika sudah login (ada SESSION pelanggan)-->
-          <?php if (isset($_SESSION['pelanggan'])): ?>
-            <li><a href="logout.php">Logout</a></li>
-          <!--jika belum login ( belum ada SESSION pelanggan)-->
-      <?php else :  ?>
-      <li class="menu-has-children"><a href="">Daftar</a>
-            <ul>
-              <li><a href="registrasi_user.php">Daftar</a></li>
-              <li><a href="login.php">Masuk</a></li>
-            </ul>
-          </li> 
-          <?php endif ?>          
-        </ul>
-      </nav>
-    </div>
-  </header>
-
+  <?php include 'navbar.php'; ?>
   <!--==========================Tampilan Awal============================-->
   <section id="hero" class="wow fadeIn">
     <div class="hero-container">
