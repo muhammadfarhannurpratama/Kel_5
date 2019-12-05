ee84ec59e1506e644084d00add85a5676e19d72e
tahajjudinfajri <kleponmns@gmail.com>
2019-12-05 08:41:45 +0700

    desain kategori

--- tanyabuku/kategori.php
+++ tanyabuku/kategori.php
@@ -2,7 +2,8 @@
 session_start();
 include 'koneksi.php'
  ?>
-<!doctype html>
+<!DOCTYPE html>
+
 <html lang="en">
 <head>
   <meta charset="utf-8">
@@ -32,13 +33,15 @@ include 'koneksi.php'
   <!-- Main Stylesheet File -->
   <link href="admin/assetss/css/style.css" rel="stylesheet">
 </head>
+
   <body>
   <?php include 'navbar.php'; ?>\
   <div class="container">
     <div class="row">
         <!-- katergori -->
-        <div class="col-2">
-            <ul class="kategori">
+        <div class="col-3">
+          <h3>Kategori</h3>
+            <ul id="ul-hitam">
                 <li>aksi</li>
                 <li>acak</li>
                 <li>komik</li>
@@ -47,7 +50,8 @@ include 'koneksi.php'
         </div>
 
         <!-- daftar buku -->
-        <div class="col-10">
+        <div class="col-9">
+        <h3>Daftar Buku</h3>
         <section id="get-started" class="padd-section text-center wow fadeInUp">
                 <div class="row">
                     <?php $ambil=$koneksi->query("SELECT * FROM produk"); ?>
@@ -58,8 +62,8 @@ include 'koneksi.php'
                         <div class="caption">
                         <h4><?php echo $perproduk['nama_produk']; ?></h4>
                         <h5><?php echo number_format($perproduk['harga_produk']); ?></h5>
-                        <a href="beli.php?id=<?php echo $perproduk['id_produk']; ?>" class="btn btn-default">Beli</a>
-                        <a href="detail.php?id=<?php echo $perproduk['id_produk']; ?>" class="btn btn-default">Detail</a>              
+                        <a href="beli.php?id=<?php echo $perproduk['id_produk']; ?>" class="btn btn-default" style="color: black">Beli</a>
+                        <a href="detail.php?id=<?php echo $perproduk['id_produk']; ?>" class="btn btn-default" style="color: black">Detail</a>              
                         </div>
                     </div>
                     </div>
@@ -79,10 +83,20 @@ include 'koneksi.php'
 
 
 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  <!-- JavaScript Libraries -->
+  <script src="admin/assetss/lib/jquery/jquery.min.js"></script>
+  <script src="admin/assetss/lib/jquery/jquery-migrate.min.js"></script>
+  <script src="admin/assetss/lib/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="admin/assetss/lib/superfish/hoverIntent.js"></script>
+  <script src="admin/assetss/lib/superfish/superfish.min.js"></script>
+  <script src="admin/assetss/lib/easing/easing.min.js"></script>
+  <script src="admin/assetss/lib/modal-video/js/modal-video.js"></script>
+  <script src="admin/assetss/lib/owlcarousel/owl.carousel.min.js"></script>
+  <script src="admin/assetss/lib/wow/wow.min.js"></script>
+  <!-- Contact Form JavaScript File -->
+  <script src="admin/assetss/contactform/contactform.js"></script>
+
+  <!-- Template Main Javascript File -->
+  <script src="admin/assetss/js/main.js"></script>
   </body>
 </html>
