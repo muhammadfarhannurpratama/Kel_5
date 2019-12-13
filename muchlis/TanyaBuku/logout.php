1b20ace2f1440044d81f66bc88d58f4f54d01688
Haroen <elmahdi869@yahoo.co.id>
2019-12-13 07:42:17 +0700

    delete project coba2

--- muchlis/TanyaBuku/logout.php
+++
@@ -1,9 +0,0 @@
-<?php 
-session_start();
-// keluar SESSION ['pelanggan']
-session_destroy();
-
-echo "<script>alert('Logout Berhasil !');</script>";
-echo "<script>location='index.php';</script>";
-
- ?>
--- muchlis/tanyabuku/admin/home.php
+++
@@ -1,1 +0,0 @@
-<h2>Selamat Datang Di Website Tanya Buku Administrator</h2>
